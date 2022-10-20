c7f022120ad36b16c0a9dc9d16953a7aa349be40
Asilbek2002 <school2924@gmail.com>
2022-10-20 22:40:41 +0500

    update

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    title: "Online Shop",
+    technologies: ["html", "css"],
+    bgColor: "#fff",
+    color: "#000",
+    borderColor: "#fff",
+    imgUrl: "https://files.fm/thumb_show.php?i=4qt9g49rf",
+  },
+  {
+    title: "Online Shop",
+    technologies: ["html", "css"],
+    bgColor: "#fff",
+    color: "#000",
+    borderColor: "#fff",
+    imgUrl: "https://files.fm/thumb_show.php?i=4qt9g49rf",
+  },
+  {
+    title: "Online Shop",
+    technologies: ["bootstrap", "git"],
+    bgColor: "#fff",
+    color: "#fff",
+    borderColor: "#fff",
+    imgUrl: "https://files.fm/thumb_show.php?i=4qt9g49rf",
+  },
+  {
+    title: "Online Shop",
+    technologies: ["bootstrap", "git"],
+    bgColor: "#fff",
+    color: "#fff",
+    borderColor: "#fff",
+    imgUrl: "https://files.fm/thumb_show.php?i=4qt9g49rf",
+  },
+  {
+    title: "Online Shop",
+    technologies: ["js", "ts"],
+    bgColor: "#fff",
+    color: "#fff",
+    borderColor: "#fff",
+    imgUrl: "https://files.fm/thumb_show.php?i=4qt9g49rf",
+  },
+];
+
+console.log(data);
+
+const result = data.filter((v) => v.technologies == v.technologies);
